1e0f00c7cfe71215af181336518022f97edb1489
Daniel Hongel and Holly DeBelser <daniel@>
2019-06-17 10:49:56 -0700

    add prototype methods to add and delete contacts.

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function AddressBook() {
 }
 
 AddressBook.prototype.addContact = function(contact){
-  contact.id = this.assisgnId();
+  contact.id = this.assignId();
   this.contacts.push(contact);
 }
 
@@ -13,7 +13,30 @@ AddressBook.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId;
 }
-//Buisness Logic for Conttacts-----
+
+AddressBook.prototype.findContact = function(id) {
+  for (var i=0; i<this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i];
+      }
+    } 
+  };
+  return false;
+}
+
+Adressbook.prototype.deleteContact = function(id) {
+  for (var i=0; i< this.contact.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        delete this.contacts[i];
+        return true;
+      }
+    }
+  };
+  return false;
+}
+//Buisness Logic for Contacts-----
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
