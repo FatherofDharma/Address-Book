b7d7a20434b46e7dc36555570c950a99535d26c1
Daniel Hongel and Josh Mabry <josh@>
2019-06-18 09:50:04 -0700

    add html and css, and logicfor our event listener

--- js/scripts.js
+++ js/scripts.js
@@ -63,7 +63,15 @@ function displayContactDetails(addressBookToDisplay) {
   contactsList.html(htmlForContactInfo);
 };
 
+function attachContactListeners() {
+  $('ul#contacts').on('click', 'li', function () {
+    console.log('The id of this <li> is' + this.id + '.');
+  });
+
+};
+
 $(document).ready(function () {
+  attachContactListeners();
   $('form#new-contact').submit(function (event) {
     event.preventDefault();
     var inputtedFirstName = $('input#new-first-name').val();
@@ -71,6 +79,6 @@ $(document).ready(function () {
     var inputtedPhoneNumber = $('input#new-phone-number').val();
     var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
     addressBook.addContact(newContact);
-    console.log(addressBook.contacts);
+    displayContactDetails(addressBook);
   });
 });
